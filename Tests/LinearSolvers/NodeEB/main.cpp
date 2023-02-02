c97895a5196e2ecb960e2c5104587cd86846895e
Weiqun Zhang <weiqunzhang@lbl.gov>
2023-02-02 00:28:07 +0000

    Prepare NodeEB test for bug report

--- Tests/LinearSolvers/NodeEB/main.cpp
+++ Tests/LinearSolvers/NodeEB/main.cpp
@@ -8,19 +8,27 @@ int main (int argc, char* argv[])
     amrex::Initialize(argc, argv);
 
     {
-        int scaling_test = 0;
-        amrex::ParmParse pp;
-        pp.query("scaling_test", scaling_test);
+        amrex::SetVerbose(0);
+        {
+            amrex::ParmParse pp("eb2");
+            pp.add("geom_type", std::string("cylinder"));
+            pp.add("cylinder_direction", 0);
+            pp.addarr("cylinder_center", std::vector<amrex::Real>{{0.0,0.7006,0.5521}});
+            pp.add("cylinder_radius", 0.125);
+            pp.add("cylinder_height", -1.0);
+            pp.add("cylinder_has_fluid_inside", 0);
+        }
+        {
+            amrex::ParmParse pp;
+            pp.add("max_iter", 15);
+        }
 
-        BL_PROFILE("main");
+        amrex::Print() << "\nThis test will pass if line 582 of amrex/Src/EB/AMReX_algoim_K is uncommented.\n\n";
+
         MyTest mytest;
         mytest.solve();
-        if (scaling_test) {
-            BL_PROFILE_REGION("SOLVE");
-            mytest.solve();
-        } else {
-            mytest.writePlotfile();
-        }
+
+        amrex::Print() << "\nSUCCESS!\n\n";
     }
 
     amrex::Finalize();
