e1be94315910b51dcbb90dbf6e29312e6f5501b7
Weiqun Zhang <weiqunzhang@lbl.gov>
2023-03-24 07:16:55 -0700

    wip

--- .github/workflows/dependencies/dependencies_ccache.sh
+++ .github/workflows/dependencies/dependencies_ccache.sh
@@ -2,19 +2,25 @@
 
 set -eu -o pipefail
 
-sudo apt-get update
+echo $pwd
 
-if [[ `lsb_release -r -s` == "20.04" ]]; then
-  sudo apt-get install -y --no-install-recommends libhiredis-dev libzstd-dev
-  wget https://github.com/ccache/ccache/releases/download/v4.5.1/ccache-4.5.1.tar.gz
-  tar xvfz ccache-4.5.1.tar.gz
-  cd ccache-4.5.1
-  mkdir build
-  cd build
-  cmake .. -DCMAKE_BUILD_TYPE=Release \
-           -DENABLE_DOCUMENTATION=OFF
-  make -j 2
-  sudo make install
-else
-  sudo apt-get install -y --no-install-recommends ccache
-fi
+wget https://github.com/ccache/ccache/releases/download/v${1}/ccache-${1}-linux-x86_64.tar.xz
+
+tar xvf ccache-${1}-linux-x86_64.tar.xz
+
+#sudo apt-get update
+#
+#if [[ `lsb_release -r -s` == "20.04" ]]; then
+#  sudo apt-get install -y --no-install-recommends libhiredis-dev libzstd-dev
+#  wget https://github.com/ccache/ccache/releases/download/v4.5.1/ccache-4.5.1.tar.gz
+#  tar xvfz ccache-4.5.1.tar.gz
+#  cd ccache-4.5.1
+#  mkdir build
+#  cd build
+#  cmake .. -DCMAKE_BUILD_TYPE=Release \
+#           -DENABLE_DOCUMENTATION=OFF
+#  make -j 2
+#  sudo make install
+#else
+#  sudo apt-get install -y --no-install-recommends ccache
+#fi
