53ba42261f514acb5fb21340400f79715c2b6908
Weiqun Zhang <weiqunzhang@lbl.gov>
2023-03-24 07:41:16 -0700

    wip

--- .github/workflows/dependencies/dependencies_ccache.sh
+++ .github/workflows/dependencies/dependencies_ccache.sh
@@ -2,6 +2,12 @@
 
 set -eu -o pipefail
 
-wget https://github.com/ccache/ccache/releases/download/v${1}/ccache-${1}-linux-x86_64.tar.xz
-tar xvf ccache-${1}-linux-x86_64.tar.xz
-sudo cp -f ccache-${1}-linux-x86_64/ccache /usr/local/bin/
+if [[ $# -eq 2 ]]; then
+  CVER=$1
+else
+  CVER=4.8
+fi
+
+wget https://github.com/ccache/ccache/releases/download/v${CVER}/ccache-${CVER}-linux-x86_64.tar.xz
+tar xvf ccache-${CVER}-linux-x86_64.tar.xz
+sudo cp -f ccache-${CVER}-linux-x86_64/ccache /usr/local/bin/
